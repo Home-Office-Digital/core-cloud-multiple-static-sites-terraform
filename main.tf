f6645a467a32fe168853d3b58d4b9d79e748f672
Chaitan Chilakamary <Chaitan.Chilakamary@digital.homeoffice.gov.uk>
2026-06-03 10:33:57 +0100

    feat(CCL-9534):adding hash

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 }
 
 module "waf" {
-  source       = "git::https://github.com/Home-Office-Digital/core-cloud-static-sites-wafv2-terraform.git?ref=0.4.11"
+  source       = "git::https://github.com/Home-Office-Digital/core-cloud-static-sites-wafv2-terraform.git?ref=c48bb30eeafeb73c517f090eb91521f50c543f8a" # 0.4.11
   waf_acl_name = "cc-static-site-${var.env_name}-acl"
   tags         = var.platform_tags
   scope        = "CLOUDFRONT"
@@ -16,7 +16,7 @@ module "cloudfront" {
 }
 
 module "static_site" {
-  source = "git::https://github.com/Home-Office-Digital/core-cloud-static-site-terraform.git?ref=0.4.0"
+  source = "git::https://github.com/Home-Office-Digital/core-cloud-static-site-terraform.git?ref=00aa5ce59d4c654b988810a85c0deaa97a872a48" # 0.4.0
 
   for_each = var.tenant_vars
 
